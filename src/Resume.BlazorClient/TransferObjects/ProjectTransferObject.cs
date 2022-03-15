20cefba66cf15f82f4217fd3431d541ac5b5dd32
drajatisme <55853547+drajatisme@users.noreply.github.com>
2022-03-15 23:26:13 +0700

    Update Projects Description

---
+++ src/Resume.BlazorClient/TransferObjects/ProjectTransferObject.cs
@@ -0,0 +1,10 @@
+﻿namespace Resume.BlazorClient.TransferObjects
+{
+    public class ProjectTransferObject
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Overview { get; set; } = string.Empty;
+        public List<string> Features { get; set; } = new List<string>();
+        public List<string> Tools { get; set; } = new List<string>();
+    }
+}
