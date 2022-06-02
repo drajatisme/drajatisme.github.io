5846ad4e8cf967c37387bc8bbce2c8d9c703ea2e
drajatisme <55853547+drajatisme@users.noreply.github.com>
2022-06-02 15:56:15 +0700

    Project Medias

--- src/Resume.BlazorClient/TransferObjects/ProjectTransferObject.cs
+++ src/Resume.BlazorClient/TransferObjects/ProjectTransferObject.cs
@@ -4,6 +4,7 @@
     {
         public string Name { get; set; } = string.Empty;
         public string Overview { get; set; } = string.Empty;
+        public List<string> Medias { get; set; } = new List<string>();
         public List<string> Features { get; set; } = new List<string>();
         public List<string> Tools { get; set; } = new List<string>();
     }
